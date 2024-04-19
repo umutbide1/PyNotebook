b80d1b9e4a3f72d47b1d128a42b793de8d7fa8b9
Umut Bide <umut.bide.564@gmail.com>
2024-04-20 01:23:03 +0300

    is ve in kullanımına dair alıştırma

---
+++ birinci_ders/ısAndInOperators.py
@@ -0,0 +1,22 @@
+#REFERANS KAVRAMLARI VE DENEMELER
+#İS VE IN OPERATOR KAVRAMLARI HAKKINDA BİLGİLER 
+
+# x=y=[1,2,3]
+# z=[1,2,3]
+
+# print(x==y)
+# print(x==z)
+# print(x is y)
+# print(x is z)
+
+x=[1,2,3]
+y=[2,4]
+
+
+print(x is y)
+#is referansların aynı mı farklı mı olduğunu belirtmek amacıyla kullanılan bir belirtec
+
+a=["apple","banana"]
+print("banana" in a) # in kullanımı ise bir dizinin içerisinde yada bir değişken de olabilir içerisinde bir 
+                     # karakterin char karakterinin yada string bir ifadenin olup olmadığının kontrolünü sağlayacaktır 
+                   
