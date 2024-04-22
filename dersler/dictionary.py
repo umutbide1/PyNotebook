c67d7f8b372a6ed5c7b0dd4e510d8bb1ce99d907
Umut Bide <umut.bide.564@gmail.com>
2024-04-23 00:35:53 +0300

    Dictionary dosyasında faal durumda olmayan kodlar düzeltilldi

--- dersler/dictionary.py
+++ dersler/dictionary.py
@@ -1,50 +1,50 @@
 #key value
 
-# sehirler=["Kocaeli","Istanbul"]
-# plakalar=[41,34]
+sehirler=["Kocaeli","Istanbul"]
+plakalar=[41,34]
 
-# print(plakalar[sehirler.index("Kocaeli")])
+print(plakalar[sehirler.index("Kocaeli")])
 
-# print(plakalar[sehirler.index("Istanbul")])
+print(plakalar[sehirler.index("Istanbul")])
 
 #bunlar dictionary olmadan 
 
-# plakalar={"Kocaeli":41,"Istanbul":34}
-# print(plakalar["Kocaeli"])
+plakalar={"Kocaeli":41,"Istanbul":34}
+print(plakalar["Kocaeli"])
 
 # plakalar["Ankara"]=6 # bu da yenisini ekliyor bu key wordslere 
 
 # print(plakalar)
 
-# users = {
-#     "UmutBide":22,    # buralarda aslında umutbide şeklinde dizinin indisi ekrana yazdırılmak istendiğinde bir yada birden fazla yere götürmesi
-#     "AhmetBurhan":21,       # istenebilir bu gibi durumlarda bu yapıyı kullanmalıyız.Mesela bir users dizisi içinde bir key ve word eşlemesi yaptık
-#     "YilmazSoke":45,        # bu eşlemeden sonra biz aşağıdaki gibi ekrana çıktı almak istediğimizde bu key bizi yanında ki bütün değerlere götürür
-#     "MustafaColak":56       # mesela bir insanın adını girerek her türlü bilgisine ulaşılmak istendiğinde bu yapı gayet mantıklı aslında 
-#     }
-# print(users["AhmetBurhan"])
+users = {
+    "UmutBide":22,    # buralarda aslında umutbide şeklinde dizinin indisi ekrana yazdırılmak istendiğinde bir yada birden fazla yere götürmesi
+    "AhmetBurhan":21,       # istenebilir bu gibi durumlarda bu yapıyı kullanmalıyız.Mesela bir users dizisi içinde bir key ve word eşlemesi yaptık
+    "YilmazSoke":45,        # bu eşlemeden sonra biz aşağıdaki gibi ekrana çıktı almak istediğimizde bu key bizi yanında ki bütün değerlere götürür
+    "MustafaColak":56       # mesela bir insanın adını girerek her türlü bilgisine ulaşılmak istendiğinde bu yapı gayet mantıklı aslında 
+    }
+print(users["AhmetBurhan"])
 
 #  #şimdi daha manalı ve daha sağlam bir yapı kuruyoruz kanka 
 
-# userss={"Bahtiyar":{
-#         "age":22,
-#         "mail":"bahtiyar@gmail.com",
-#         "adress":"Sivas"
-#         },
-#         "Bahadir":{
-#             "age":24,
-#             "mail":"bahadir@gmail.com",
-#             "adress":"Ordu"
-#         },
-#         "Umut":{
-#         "age":21,
-#         "mail":"umut@gmail.com",
-#         "adress":"Kocaeli"
-#         },
-#         }
+userss={"Bahtiyar":{
+        "age":22,
+        "mail":"bahtiyar@gmail.com",
+        "adress":"Sivas"
+        },
+        "Bahadir":{
+            "age":24,
+            "mail":"bahadir@gmail.com",
+            "adress":"Ordu"
+        },
+        "Umut":{
+        "age":21,
+        "mail":"umut@gmail.com",
+        "adress":"Kocaeli"
+        },
+        }
 
-# print(userss["Umut"])
-# print(userss["Umut"]["age"]) #ikinci kısım spesifik bir istek 
+print(userss["Umut"])
+print(userss["Umut"]["age"]) #ikinci kısım spesifik bir istek 
 
 #sevdim burayı baya yararlı duruyor açıkçası
 
