1cce178ccccae3963a69cfb5b72e9ff6940683a8
Umut Bide <umut.bide.564@gmail.com>
2024-04-26 12:39:47 +0300

    Fonksiyon örnek 1

---
+++ dersler/functionPratic.py
@@ -0,0 +1,9 @@
+# GONDERILEN BİR KELİMEYİ BELİRTİLEN KEZ EKRANDA GOSTEREN FONKSIYONU YAZIN
+
+def ekranaYazdir(yazdirilacakDeger,yazdirilmaMiktari):
+    while yazdirilmaMiktari>0:
+        yazdirilmaMiktari=yazdirilmaMiktari-1
+        print(yazdirilacakDeger)
+
+ekranaYazdir("Umut",10)
+
