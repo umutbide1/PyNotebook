25a622cdbaf03b63dc8aa3536c29f5f498d7369b
Umut Bide <umut.bide.564@gmail.com>
2024-04-27 15:48:46 +0300

    Bankamatik uygulamasi 1

---
+++ dersler/bankamatikUyg.py
@@ -0,0 +1,36 @@
+umutHesap = {
+    "ad": "Umut Bide",
+    "hesapNo": "564",
+    "bakiye": 5000,
+    "ekHesap": 2000
+}
+
+beyzaHesap = {
+    "ad": "Beyzanur Çam",
+    "hesapNo": "276",
+    "bakiye": 9000,
+    "ekHesap": 55000
+}
+
+hesaplar = [umutHesap, beyzaHesap]
+
+def paraCek(hesapNumarasi, miktar):
+    for hesap in hesaplar:
+        if hesap["hesapNo"] == str(hesapNumarasi):
+            print("Sayin", hesap["ad"], "Hosgeldiniz.")
+            bakiye = hesap["bakiye"]
+            ekHesap = hesap["ekHesap"]
+            toplamBakiye = bakiye + ekHesap
+            if miktar > toplamBakiye:
+                print("Hesabinizde yeterli bakiye bulunmamaktadir")
+                return False
+            else:
+                hesap["bakiye"] -= miktar
+                print(f"Para cekme isleminiz tamamlandi. Islem sonrasi bakiyeniz: {hesap['bakiye']}")
+                return True
+    print("Hesap Bulunamadi..")
+    return False
+
+paraCek(564, 5500)
+
+#buna eklemeler yapılabilir 
