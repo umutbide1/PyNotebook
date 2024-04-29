54fac1baf6807e0fcc5b343bf9488976e0681433
Umut Bide <umut.bide.564@gmail.com>
2024-04-29 20:29:40 +0300

    Nesne Yönelimli Programlama Başlangıç

---
+++ dersler/objectOriantedProgramming.py
@@ -0,0 +1,16 @@
+# NEYMİŞ ÖĞRENİYORMUSUZ
+# CLASS TANIMI VE İNSTANCE(OBJE) KAVRAMINI BİRAZ ANLAMAMIZ LAZIMMIŞ
+
+lst=[1,2,3]
+lst2=[1,2,3]
+print(type(lst))
+
+
+# aslında burada bir bir list oluşturunca bir list classından bir nesne üretiyoruz ve kullanıyoruz
+
+ 
+# sınıf oluşutucaz ve bu sınıf içerisinden objeler üreteceğim
+#
+# 
+# 
+#
