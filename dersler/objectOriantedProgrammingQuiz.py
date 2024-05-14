b533af19184b55de2622135571c2d223a7d77de1
Umut Bide <umut.bide.564@gmail.com>
2024-05-14 21:58:23 +0300

    Python derslerin devamı

--- dersler/objectOriantedProgrammingQuiz.py
+++ dersler/objectOriantedProgrammingQuiz.py
@@ -1,3 +1,4 @@
 # Question sınıfi
 # quiz sınıfı olacak
 # quiz sınıfı soruları alacak ve sırayla kullanıcıya gösterecek ve sonucunda kaçını bildi bilemedi ekrana yazacak 
+# Burayı tam anlamadım ama kursa devam ediyorum. 
