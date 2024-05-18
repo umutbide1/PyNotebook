0d40d60d1b14520f32a2c10679e803c7c4c92b88
Umut Bide <umut.bide.564@gmail.com>
2024-05-19 01:44:35 +0300

    modül tanımlama işlemleri

---
+++ dersler/moduls.py
@@ -0,0 +1,21 @@
+# Modülden kasıt kütüphaneler
+#
+#
+#
+# Kendi yazdığımız kütüphaneler
+# Hazır Modüller (Numpy,Pandas) gibi 
+#   a-)Standart Kütüphane modülleri
+#   b-)Üçüncü şahış Modüller
+
+# pypi.org     Kütüphanelerin işlevlerini ve fonskiyonlarını tanıtan harika bir site
+
+import math as islem # burada mesela kütüphaneye kendim isim verdim 
+
+value= dir(islem)
+value=help(islem)
+print(value)
+
+
+from math import * # burada mesela math kütüphanesinden bütün modülleri çekiyor ayrı ayrı da çekebiliyormuşuz
+
+from math import sqrt,factorial # parça parça import muhabbeti
