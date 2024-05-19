2346d4be6ccd7ca89c95db9f0aa64c77d2e26951
Umut Bide <umut.bide.564@gmail.com>
2024-05-19 18:21:27 +0300

    mainde cagirilabilir sekilde bir modul yazdik ve simdi bu modulu kullanacagız

---
+++ dersler/definationModuls.py
@@ -0,0 +1,30 @@
+"""
+modül hakkinda bilgilendirme
+
+"""
+
+
+number=10
+
+numbers=[1,2,3]
+
+person= {
+    "name":"Ali",
+    "age":"25",
+    "city":"Istanbul"
+    
+    
+    
+}
+
+def function(x):
+    """
+    Fonksiyon hakkinda bilgilendirme
+    
+    """
+    print(f"x:{x}")
+    
+    
+class Person:
+    def speak(self):
+        print("i am speaking...")
