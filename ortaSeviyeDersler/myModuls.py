606b774fd839cfbca79d02b8bacd94f34e6a4247
Umut Bide <umut.bide.564@gmail.com>
2024-05-20 11:44:45 +0300

    Enteresan hatalar ve Modül tanımlamaları sonrasında eğitimin modul kısmı bitti

---
+++ ortaSeviyeDersler/myModuls.py
@@ -0,0 +1,30 @@
+"""
+modül hakkinda bilgilendirme
+
+"""
+print("Modul hazirlandi")
+
+number=10
+
+numbers=[1,2,3]
+
+person= {
+    "name":"Ali",
+    "age":"25",
+    "city":"Istanbul"
+    
+    
+    
+}
+
+def function(x):
+    """
+    Fonksiyon hakkinda bilgilendirme
+    
+    """
+    print(f"x:{x}")
+    
+    
+class Person:
+    def speak(self):
+        print("i am speaking...")
--- ortaSeviyeDersler/test.py
+++ ortaSeviyeDersler/test.py
@@ -1,5 +1,6 @@
-import definationModuls
+import myModuls
 import numpy as np  # inanılmaz garip bir hata çözdüm 
                     # dosyada içerisinde numbers adında bir .py uzantılı dosya olunca hata veriyordu kütüphane ismini değiştidim düzeldi
                     # içersindeki integral kütüphanesi de bu değişkeni ya da modülü kullanıyor zannedersem ondan dolayı böyle bir hata var  
-result= help(definationModuls)
+result= help(myModuls)
+result= help(np)
