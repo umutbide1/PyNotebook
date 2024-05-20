05abdf6b0cddc937f77027428096ff57a425a865
Umut Bide <umut.bide.564@gmail.com>
2024-05-20 15:30:55 +0300

    Son durum Pythonda hata ve hata ayıklama kısmında kaldım

--- ortaSeviyeDersler/test.py
+++ ortaSeviyeDersler/test.py
@@ -1,6 +1,10 @@
 import myModuls
+import numbers
 import numpy as np  # inanılmaz garip bir hata çözdüm 
                     # dosyada içerisinde numbers adında bir .py uzantılı dosya olunca hata veriyordu kütüphane ismini değiştidim düzeldi
                     # içersindeki integral kütüphanesi de bu değişkeni ya da modülü kullanıyor zannedersem ondan dolayı böyle bir hata var  
 result= help(myModuls)
 result= help(np)
+ 
+
+result=help(numbers)
